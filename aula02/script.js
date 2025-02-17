3ac194d3c86662f33068a2fc0ad99ef28e59c50d
Mayara de Oliveira Sant'Anna <159502755+mayarasantanna2@users.noreply.github.com>
2025-02-17 12:38:03 -0300

    Add files via upload

---
+++ aula02/script.js
@@ -0,0 +1,76 @@
+console.log("Exercício 01")
+
+var a = 10;
+var b = 20;
+
+console.log (b)
+
+b = 5;
+
+console.log (a,b)
+
+
+console.log("====================================================")
+
+console.log("Exercício 02")
+
+var a2 = 30;
+var b2 = 20;
+var c2 = a2 + b2;
+
+console.log (c2)
+
+b2 = 10;
+
+console.log (b2,c2)
+
+var c2 = a2 + b2;
+
+console.log (a2,b2,c2)
+
+
+console.log("====================================================")
+
+console.log("Exercício 03")
+
+var a3 = 10;
+var b3 = 20;
+var c3 = a3;
+var b3 = c3;
+var a3 = b3;
+
+console.log (a3,b3,c3)
+
+
+console.log("====================================================")
+
+console.log("Exercício 04")
+
+var a4 = 10;
+var b4 = a4 + 1;
+var a4 = b4 + 1;
+var b4 = a4 + 1;
+
+
+console.log (a4)
+
+a4 = b4 + 1;
+
+console.log(a4,b4)
+
+
+
+
+console.log("====================================================")
+
+var n1= 2;
+var n2= 4;
+var n3= 6;
+
+m = (n1 + n2 + n3)/3;
+
+console.log(m)
+
+mf = ((n1*2) + (n2*3) + (n3*5))/10
+
+console.log("A média final do aluno é: ",mf)
